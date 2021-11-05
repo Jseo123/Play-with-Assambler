43af36c4f760d73a7b02d7e063ff793d04aa045f
jseo123 <jose123135@gmail.com>
2021-11-05 21:57:44 +0100

    Commented currently created functions git push

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ const mainContainer = document.querySelector(".gameContainer")
 const game = document.querySelector(".game")
 const scoreSection = document.querySelector(".scoreSection")
 
-
+// Calls the function on windows load to create the username input and button.
 window.onload = (e => {
     callMe();
 })
 
 
 
-
+//creates the first phase html.
 function callMe () {
     var username  = document.createElement("h1")
     username.innerHTML = "Username"
@@ -24,20 +24,34 @@ function callMe () {
     startGame.setAttribute("class", "startButton")
    //startGame.setAttribute("class", "iamabutton")
     game.appendChild(startGame);
+
+    //calls function to delete the HTML created here.
 deleteElements(startGame, inputField, username);
 }
 
+//deletes html elements.
 function deleteElements(startGame, inputField, username) {
     
     startGame.addEventListener("click", (e) => {
         game.removeChild(username);
         game.removeChild(inputField);
         game.removeChild(startGame);
+        //calls on function to create game button.
         gameCreate()
         })};
 
+        //creates game button
         function gameCreate() {
             let gameButton = document.createElement("button");
             gameButton.innerHTML = "Start game";
             game.appendChild(gameButton);
+            //calls function to change the inner HTML of gameButton and adds
+            //counter to pass to scores.
+            createClickMe(gameButton);
+        }
+
+        function createClickMe(gameButton) {
+            gameButton.addEventListener("click", (e) => {
+                gameButton.innerHTML = "Click me!";
+            })
         }
