9fa332a5d7d8a6fa43fc7a52884f4a71e6ca659c
jseo123 <jose123135@gmail.com>
2021-11-05 16:59:34 +0100

    created logic for function jumping to make dynamic site

--- script.js
+++ script.js
@@ -2,8 +2,33 @@ const mainContainer = document.querySelector(".gameContainer")
 const game = document.querySelector(".game")
 const scoreSection = document.querySelector(".scoreSection")
 
-window.onload = (e) => {
+
+window.onload = (e => {
+    callMe();
+})
+
+
+
+
+function callMe () {
     var username  = document.createElement("h1")
-    username.innerHTML = "username"
-    game.appendChild(username)
+    username.innerHTML = "Username"
+    game.appendChild(username);
+
+    var inputField = document.createElement("input")
+    inputField.setAttribute("type", "text")
+    game.appendChild(inputField)
+
+    var startGame = document.createElement("button")
+    startGame.innerHTML = "Start Game"
+    startGame.setAttribute("class", "startButton")
+   //startGame.setAttribute("class", "iamabutton")
+    game.appendChild(startGame);
+changeColor(startGame);
 }
+
+function changeColor(startGame) {
+    
+    startGame.addEventListener("click", (e) => {
+        startGame.style.color = "green";
+    } ) }
