6eb4dce1abf31c0105a601a379214a40de109736
Carla <carlacardozo990@gmail.com>
2021-11-08 13:30:18 +0000

    css

--- script.js
+++ script.js
@@ -1,69 +1,71 @@
-const mainContainer = document.querySelector(".gameContainer")
-const game = document.querySelector(".game")
-const scoreSection = document.querySelector(".scoreSection")
+const mainContainer = document.querySelector(".gameContainer");
+const game = document.querySelector(".game");
+const scoreSection = document.querySelector(".scoreSection");
 
 // Calls the function on windows load to create the username input and button.
-window.onload = (e => {
-    callMe();
-})
+window.onload = (e) => {
+  callMe();
+};
 //creates the first phase html.
-function callMe () {
-    var username  = document.createElement("h3")
-    username.innerHTML = "Username"
-    username.setAttribute("class", "gameInput")
-    game.appendChild(username);
+function callMe() {
+  var username = document.createElement("h3");
+  username.innerHTML = "Username";
+  username.setAttribute("class", "gameInput");
+  game.appendChild(username);
 
-    var inputField = document.createElement("input")
-    inputField.setAttribute("type", "text")
-    username.appendChild(inputField)
+  var inputField = document.createElement("input");
+  inputField.setAttribute("type", "text");
+  username.appendChild(inputField);
 
-    var startGame = document.createElement("button")
-    startGame.innerHTML = "Start Game"
-    startGame.setAttribute("class", "startButton")
-   //startGame.setAttribute("class", "iamabutton")
-    username.appendChild(startGame);
+  var startGame = document.createElement("button");
+  startGame.innerHTML = "Start Game";
+  startGame.setAttribute("class", "startButton");
+  //startGame.setAttribute("class", "iamabutton")
+  username.appendChild(startGame);
 
-    //calls function to delete the HTML created here.
-deleteElements(startGame, inputField, username);
+  //calls function to delete the HTML created here.
+  deleteElements(startGame, inputField, username);
 }
 
 //deletes html elements.
 function deleteElements(startGame, inputField, username) {
-    
-    startGame.addEventListener("click", (e) => {
-        game.removeChild(username);
-        //calls on function to create game button.
-        gameCreate()
-        })};
+  startGame.addEventListener("click", (e) => {
+    game.removeChild(username);
+    //calls on function to create game button.
+    gameCreate();
+  });
+}
 
-        //creates game button
-        function gameCreate() {
-            let gameButton = document.createElement("button");
-            gameButton.innerHTML = "Start game";
-            game.appendChild(gameButton);
-            //calls function to change the inner HTML of gameButton and adds
-            //counter to pass to scores.
-            createClickMe(gameButton);
-        }
+//creates game button
+function gameCreate() {
+  let gameButton = document.createElement("button");
+  gameButton.innerHTML = "Start game";
+  game.appendChild(gameButton);
+  //calls function to change the inner HTML of gameButton and adds
+  //counter to pass to scores.
+  createClickMe(gameButton);
+}
 
-        function createClickMe(gameButton) {
-            //Starts game
-            let score = -1;
-            gameButton.addEventListener("click", (e) => {
-                gameButton.innerHTML = "Click me!";
-                //adds one to the index for every click
-                score++;
-                //Stops the game and creates last page. The if is to make sure it executes only once.
-                if (score < 1) {
-                setTimeout(() => {
-                    showScore(score, gameButton)
-                }, 3000);};
-            })
-        }
+function createClickMe(gameButton) {
+  //Starts game
+  let score = -1;
+  gameButton.addEventListener("click", (e) => {
+    gameButton.innerHTML = "Click me!";
+    //adds one to the index for every click
+    score++;
+    //Stops the game and creates last page. The if is to make sure it executes only once.
+    if (score < 1) {
+      setTimeout(() => {
+        showScore(score, gameButton);
+      }, 3000);
+    }
+  });
+}
 
-   function showScore(score, gameButton) {
-       game.removeChild(gameButton);
-       const scoreShow = document.createElement("p")
-       scoreShow.innerHTML = "Congratulations you got " + score.toString() + " " + "points";
-       game.appendChild(scoreShow);
-   }     
+function showScore(score, gameButton) {
+  game.removeChild(gameButton);
+  const scoreShow = document.createElement("p");
+  scoreShow.innerHTML =
+    "Congratulations you got " + score.toString() + " " + "points";
+  game.appendChild(scoreShow);
+}
